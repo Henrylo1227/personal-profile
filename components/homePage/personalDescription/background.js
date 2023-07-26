16b7ca98488d571312880e2aa22a072ee0445af1
Henrylo1227 <52083095+Henrylo1227@users.noreply.github.com>
2023-07-26 17:47:55 +0100

    finalize styling of home:personalDescription

--- components/homePage/personalDescription/background.js
+++ components/homePage/personalDescription/background.js
@@ -1,18 +1,11 @@
-import { Trispace } from "next/font/google";
 import Image from "next/image";
 
 export default function Background() {
     return (
-        <section className="hidden w-full h-full">
-            <div className="hidden md:flex">
-                <Image src='/images/vectorAssets/thinking.png' width={400} height={300} className="rotate-[19deg] absolute top-32 right-[10%]"></Image>
-                <Image src='/images/vectorAssets/puzzle_1.png' width={275} height={275} className="absolute bottom-[7%] right-[15%]"></Image>
-                <Image src='/images/vectorAssets/glass.png' width={300} height={150} className="absolute rotate-[17deg] bottom-[35%] left-[28%]"></Image>
-                <Image src='/images/vectorAssets/brain.png' width={300} height={300} className="absolute bottom-[5%] left-[33%]"></Image>
-            </div>
-            <div className=" fixed brightness-50 md:top-[30%] md:flex md:absolute mdbrightness-100 md:right-[30%] md:rounded-[50%]">
-                <Image src='/images/homePage/profile-bg.jpeg' width={400} height={400} />
-             </div>
-        </section>
+        <div className="absolute w-full h-full top-5">
+            <Image src='/images/homePage/profile-bg.jpeg' fill={true}
+                className=" object-cover brightness-[60%]"
+            />
+        </div>
     );
 }
--- components/homePage/personalDescription/personalDescription.js
+++ components/homePage/personalDescription/personalDescription.js
@@ -11,12 +11,11 @@ export default function PersonalDescription() {
 
   return (
     <section>
-      <Background/>
-      <div className='description-container absolute top-1/4 p-3 md:top-1/3 md:left-[15%]'>
+      <div className='description-container absolute top-1/4 p-3 md:top-1/3 md:left-[15%] z-10'>
         <div className='text-5xl my-2'>
           <div className={limeLight.className}>{heading}</div>
         </div>
-        <div className='text-3xl my-2 mx-3 italic font-light brightness-75'>
+        <div className='text-3xl my-2 mx-3 italic font-semibold brightness-[75%]'>
           <div>{subHeading}</div>
         </div>
         <div className='text-xl my-1 mx-4 font-light max-w-sm'>
