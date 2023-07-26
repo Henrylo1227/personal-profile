b9b2fef00b6e13c9fad038add9de43ff3c1f94ee
Henrylo1227 <52083095+Henrylo1227@users.noreply.github.com>
2023-07-26 17:04:16 +0100

    make description responsive

--- components/homePage/personalDescription/personalDescription.js
+++ components/homePage/personalDescription/personalDescription.js
@@ -12,17 +12,17 @@ export default function PersonalDescription() {
   return (
     <section>
       <Background/>
-      <div className='description-container absolute top-1/3 max-w-[300px] left-[15%]'>
+      <div className='description-container absolute top-1/4 p-3 md:top-1/3 md:left-[15%]'>
         <div className='text-5xl my-2'>
           <div className={limeLight.className}>{heading}</div>
         </div>
-        <div className='text-3xl my-2 italic font-light'>
+        <div className='text-3xl my-2 mx-3 italic font-light brightness-75'>
           <div>{subHeading}</div>
         </div>
-        <div className='text-xl my-1 font-light'>
+        <div className='text-xl my-1 mx-4 font-light max-w-sm'>
           <div>{text}</div>
         </div>
-        <div className='mt-[20%]'>
+        <div className='flex justify-center mt-[20%] md:justify-start'>
           <DiscoverMeBtn/>
       </div>
       </div>
